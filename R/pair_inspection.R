6caf9689ba0a58199132b51a43107432689bc54f
Morten Dall <mortendall1988@gmail.com>
2021-08-31 16:09:55 +0200

    Added individual breeding pair inspection

--- R/pair_inspection.R
+++ R/pair_inspection.R
@@ -6,7 +6,8 @@ pair_inspection_ui <- function(id, data_sheet){
       shiny::uiOutput(ns("BreedingPairSelector"))
     ),
     mainPanel(
-      
+      shiny::plotOutput(ns("BreedingPairGraph"),
+                        )
     ))
 }
 
@@ -19,6 +20,25 @@ pair_inspection <- function(id, data_sheet){
                                                                     label = "Breeding Pair",
                                               choices = unique(data_sheet$data$Breeding_pair))
                                               })
+        output$BreedingPairGraph <- renderPlot({
+          plot_data <- data_sheet$data
+           
+          if (!is.null(plot_data)) {
+            plot_data <-
+              plot_data %>% dplyr::mutate(Litter_size = Male_pups + Female_pups)
+            plot_data <- plot_data %>% 
+              dplyr::filter(Breeding_pair == input$BreedingPair)
+            Breeding_chart <-
+              ggplot2::ggplot(plot_data,
+                              aes(x = Litter_date, 
+                                  y = Litter_size, 
+                                  color = Breeding_pair)) + 
+              geom_line() +
+              geom_point(size = 8)
+            Breeding_chart
+          }
+          
+        })
       
       
       
